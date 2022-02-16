bd07784c8bd835c26b1fdd39639e3e6a0e27c75a
Fabricyo Barreto <fabriciobarreto@hotmail.com>
2022-02-16 07:16:56 -0300

    theme/resources: It was add fonts and the global theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {ThemeProvider} from 'styled-components/native';
 import {Home} from './src/screens/Home';
+import theme from './src/theme';
 
 const App = () => {
   return (
     <GestureHandlerRootView style={styles.root}>
-      <Home />
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
     </GestureHandlerRootView>
   );
 };
---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+export default {
+  colors: {
+    primary: '#5BA367',
+    primary_dark: '#1e732a',
+    primary_light: '#b1d1ab',
+    success: '#12a454',
+    success_light: 'rgba(18, 164, 84, 0.5)',
+    attention: '#ff1c1c',
+    blue: '#2b5bfa',
+    shape: '#ffffff',
+    text: '#0f0f0f',
+    text_light: '#a3aaa4bb',
+    text_dark: '#cbcbcb',
+    background: '#f4f9f5',
+    bottomSheetList: '#f9fffb',
+    gray: '#e9eeea',
+  },
+  fonts: {
+    title: 'Gideon Roman',
+    text_bold: 'Lato-Black',
+    text: 'Lato-Regular',
+  },
+};
